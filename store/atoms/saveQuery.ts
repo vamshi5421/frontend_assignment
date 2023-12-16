b963da6db9671a798dddf02281c0287337be9bf3
vamshi5421 <75656830+vamshi5421@users.noreply.github.com>
2023-12-17 01:01:11 +0530

    adding saved queries

--- store/atoms/saveQuery.ts
+++ store/atoms/saveQuery.ts
@@ -4,5 +4,48 @@ import { QueryInfo } from "@/types/query"
 
 export const savedQueryState = atom<QueryInfo[]>({
   key: "savedQueryState",
-  default: [],
+  default: [
+    {
+      id: 1,
+      query: `    -- select db to query upon.
+      -- type your query
+      
+      select * from courses;
+      -- select * from products;
+      
+      -- save your query`,
+      queryName: "courseQuery",
+      note: "courses notes.",
+      dbName: "course",
+      tableName: "course",
+    },
+    {
+      id: 2,
+      query: `    -- select db to query upon.
+      -- type your query
+      
+      -- select * from courses;
+       select * from products;
+      
+      -- save your query`,
+      queryName: "productQuery",
+      note: "product notes.",
+      dbName: "product",
+      tableName: "product",
+    },
+    {
+      id: 3,
+      query: `    -- select db to query upon.
+      -- type your query
+      
+      -- select * from courses;
+       select * from products;
+      
+      -- save your query`,
+      queryName: "ordersQuery",
+      note: "orders notes.",
+      dbName: "order",
+      tableName: "order",
+    },
+  ],
 })
